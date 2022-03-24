89f57a358d78d8705d7b91f9fc7f41d99ad5d54b
luchikAR <arbelenov@yandex.ru>
2022-03-24 14:40:15 +0300

    start v1.1

--- main.cpp
+++ main.cpp
@@ -10,7 +10,9 @@
 
 int main(int argc, char *argv[])
 {
-    int                 status;
+    // ------------------------------------------------
+    // заполняем структуру addrinfo и связный список
+    int                 status;     // проверка возвращаемого рез.
     struct addrinfo     hints;
     struct addrinfo     *servinfo;  // указатель на результаты
 
@@ -18,15 +20,22 @@ int main(int argc, char *argv[])
     hints.ai_family = AF_UNSPEC;     // неважно, IPv4 или IPv6
     hints.ai_socktype = SOCK_STREAM; // TCP stream-sockets
     hints.ai_flags = AI_PASSIVE;     // заполните мой IP-адрес за меня
-    if ((status = getaddrinfo(NULL, "3490", &hints, &servinfo)) != 0) {
+    
+    // getaddrinfo  (имя хоста или IP-адрес для соединения #указать argv[1]!#, 
+    //              сервис который может быть номером порта («80») или названием сервиса («http»), 
+    //              указатель на struct addrinfo, которую вы уже заполнили минимальной информацией,
+    //              ) 
+    status = getaddrinfo("www.example.com", "http", &hints, &servinfo);
+    if (status != 0)
+    {
         fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
-        exit(1);
+        return (1);
     }
     // servinfo теперь указывает на связанный список на одну или больше структуру <i>addrinfo</i>// … Делаем что-то, где используем структуру <i>addrinfo</i> ….
+    
+    int sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol); // создаем сокет
 
-    fprintf(stderr, "servinfo->ai_family: %d\n", servinfo->ai_family);
-    fprintf(stderr, "servinfo->ai_socktype: %d\n", servinfo->ai_socktype);
-    fprintf(stderr, "servinfo->ai_protocol: %d\n", servinfo->ai_protocol);
+    bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen);
 
     freeaddrinfo(servinfo); // и освобождаем связанный список
 
