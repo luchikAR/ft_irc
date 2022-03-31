008f189086cefea0fa1b18a9479e34325a9f9058
luchikAR <arbelenov@yandex.ru>
2022-03-31 11:08:17 +0300

    nothing new

---
+++ server.cpp
@@ -0,0 +1,33 @@
+
+
+#include "Server.hpp"
+
+Server::Server(const char *port, const char *pass) {
+    this->port = atoi(port);
+	this->pass = atoi(pass);
+
+    memset(&hints, 0, sizeof(hints)); // убедимся, что структура пуста
+    hints.ai_family     = AF_UNSPEC;     // неважно, IPv4 или IPv6
+    hints.ai_socktype   = SOCK_STREAM; // TCP stream-sockets
+    hints.ai_flags      = AI_PASSIVE;     // заполните мой IP-адрес за меня
+}
+
+int Server::start(void)
+{
+
+    /* old version
+    status = getaddrinfo("www.example.com", "http", &hints, &servinfo);
+    if (status != 0)
+    {
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+        return (1);
+    }
+    // servinfo теперь указывает на связанный список на одну или больше структуру <i>addrinfo</i>// … Делаем что-то, где используем структуру <i>addrinfo</i> ….
+    
+    int sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol); // создаем сокет
+
+    bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen);
+
+    freeaddrinfo(servinfo); // и освобождаем связанный список
+    */
+}
