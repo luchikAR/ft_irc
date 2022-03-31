d19b1e05c2262fb37e58b00b2a402bb54839c8d2
luchikAR <arbelenov@yandex.ru>
2022-03-31 15:26:55 +0300

    nothing

--- example_from_inet/server.cpp
+++ example_from_inet/server.cpp
@@ -92,15 +92,14 @@ int main(void)
     printf("server: waiting for connections…\n");
     while(1)
     {  // главный цикл accept()
-        sin_size = sizeof their_addr;
+        sin_size = sizeof (their_addr);
         new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
         if (new_fd == -1)
         {
             perror("accept");
             continue;
         }
-        inet_ntop(their_addr.ss_family,
-        get_in_addr((struct sockaddr *)&their_addr), s, sizeof(s) );
+        inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr), s, sizeof(s) );
         printf("server: got connection from %s\n", s);
 
         if (!fork()) // тут начинается дочерний процесс
